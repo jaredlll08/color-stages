dd18685b1ab149fc5b1a4197d754a4a883fbd441
Jared <jaredlll08@gmail.com>
2024-06-12 13:32:25 -0600

    Initial commit

---
+++ src/main/java/com/blamejared/colorstages/ColorStages.java
@@ -0,0 +1,16 @@
+package com.blamejared.colorstages;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Color_stages.MODID)
+public class Color_stages {
+
+    public static final String MODID = "color_stages";
+    public static final Logger LOG = LogUtils.getLogger();
+
+    public Color_stages() {
+    }
+}
